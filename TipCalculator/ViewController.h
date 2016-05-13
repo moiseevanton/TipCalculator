65b3cc9050aa148a62325188282698d1f9b38bf4
moiseevanton <athidif94@gmail.com>
2016-05-13 11:47:52 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Anton Moiseev on 2016-05-13.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
